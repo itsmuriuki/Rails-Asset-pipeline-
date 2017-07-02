1262457101edc256c2bdd005a8f0fd598ca5ffae
Gerald Munyingi <grdmuriuki@gmail.com>
2017-07-02 17:47:27 +0000

    initial ui

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,8 @@
+class DashboardController < ApplicationController
+  def index
+  end
+
+  def compose
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+
   	get 'welcome/index'
 
  	root 'welcome#index'
+
+ 	get 'dashboard/compose'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
